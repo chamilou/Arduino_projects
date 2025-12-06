0cec233bfb9438e7cacf16658bdb663fef561340
chamilou <chamilou@gmail.com>
2025-12-06 08:58:25 +0100

    remoed some olines

--- sketch.ino
+++ sketch.ino
@@ -5,14 +5,6 @@ const long interval2 = 1500;
 
 void loop() {
     unsigned long currentMillis = millis();
-    
-    if (currentMillis - previousMillis1 >= interval1) {
-        previousMillis1 = currentMillis;
-        // Toggle LED1
-    }
-    
-    if (currentMillis - previousMillis2 >= interval2) {
-        previousMillis2 = currentMillis;
-        // Toggle LED2
+   
     }
 }
